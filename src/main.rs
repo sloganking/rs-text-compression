0847967753fce3ec3b83b655c5885f1bdbe01fab
Logan King <seanloganking@gmail.com>
2021-12-15 00:26:03 -0400

    working decompression

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 // initialization
     // import dependancies
         use std::fs::File;
@@ -6,9 +7,8 @@
         use serde_json::json;
 
 // function declarations
-    fn encode_word(word: &str){
-
-    }
+    // fn encode_word(word: &str){
+    // }
 
     fn is_valid_capitalization(word: &str) -> bool{
         if word == word.to_uppercase(){
@@ -23,28 +23,84 @@
             return true
         }
         // if first char is uppercase, and rest are lowercase
-        if &word[0..1] == word[0..1].to_uppercase() && &word[1..] == &word[1..].to_lowercase(){
+        if word[0..1] == word[0..1].to_uppercase() && word[1..] == word[1..].to_lowercase(){
             return true
         }
-        return false
+        false
     }
 
 
-    fn decompress(compressed_bytes: Vec<u8>) -> String{
+    fn decompress(compressed_bytes: Vec<u8>, index_to_word: HashMap<u32, String>) -> String{
 
-        return "Hi".to_string();
+        let mut decompressed_text: String = String::new();
+        let mut i = 0;
+        while i < compressed_bytes.len(){
+            // if next three bytes are a compressed word
+            if compressed_bytes[i] & 128 != 0{
+                // get spaces
+                    let left_space = compressed_bytes[i] & 16 != 0;
+                    let right_space = compressed_bytes[i] & 8 != 0;
+
+                // get capitalization
+                    // 00 = allLower
+                    // 01 = allUpper
+                    // 10 = firstCharUpper restLower
+                    let capstate = (compressed_bytes[i] >> 5) & 3;
+                // get word index
+                    // convert bytes to u32 number
+                    let mut word_index: u32 = ((compressed_bytes[i] as u32) << 16) | ((compressed_bytes[i + 1] as u32) << 8) | compressed_bytes[i + 2] as u32;
+                    // limit to 19 bits
+                    word_index = word_index & 0b000001111111111111111111;
+
+                // build text
+                    let mut word = index_to_word[&word_index].clone();
+                    // capitalize word
+                        if capstate == 0{
+                            // is this necessary?
+                            word = word.to_lowercase();
+                        }else if capstate == 1{
+                            word = word.to_uppercase();  
+                        }else if capstate == 2{
+                            // make first char uppercase
+                            let ch = word.chars().nth(0).unwrap();
+                            let mut first_char = String::new();
+                            first_char.push(ch);
+
+                            word = first_char + &word[1..].to_lowercase();
+                        }else{
+                            panic!("invalid capitalization state")
+                        }
+                    // append any spacing
+                        let mut text = word;
+                        if left_space {
+                            text = String::from(" ") + &text;
+                        }
+                        if right_space{
+                            text =  text.to_string() + " ";
+                        }
+                    // store decompressed text
+                        decompressed_text = decompressed_text + &text;
+                    
+                    // update current byte
+                        i += 3;
+            }else{
+                decompressed_text.push(compressed_bytes[i] as char);
+                i += 1;
+            }
+        }
+        decompressed_text
     }
 
 fn main() {
 
-    // prepair word_table
+    // prepair word_to_index
         // retrieve words from json
             let mut file = File::open("./english-words/words.txt").expect("Failed to open file");
             let mut contents = String::new();
             file.read_to_string(&mut contents).expect("Failed to read to string");
 
         // divide file by lines
-            let lines: Vec<&str> = contents.split("\n").collect();
+            let lines: Vec<&str> = contents.split('\n').collect();
 
         // remove return character from strings
             let mut fixed_lines: Vec<String> = Vec::new();
@@ -58,13 +114,18 @@ fn main() {
                 line.make_ascii_lowercase();
             }
 
-        // put words into word_table (for faster searching)
-            let mut word_table: serde_json::Value = serde_json::from_str("{}").expect("JSON was not well-formatted");
-            let word_table = word_table.as_object_mut().unwrap();
+        // put words into word_to_index (for faster searching)
+            // create word_to_index table
+                let mut word_to_index: serde_json::Value = serde_json::from_str("{}").expect("JSON was not well-formatted");
+                let word_to_index = word_to_index.as_object_mut().unwrap();
+            // create index_to_word table
+                let mut index_to_word: HashMap<u32, String> = HashMap::new();
+
+
             for (i, line) in fixed_lines.iter().enumerate() {
-                word_table.insert(line.to_string(), json!(i));
+                word_to_index.insert(line.to_string(), json!(i));
+                index_to_word.insert(i as u32, line.to_string());
             }
-            let word_table = word_table;
 
         println!("done pre-processing");
 
@@ -124,16 +185,13 @@ fn main() {
             let mut intermediate_compressed_bytes: Vec<Vec<u8>> = Vec::new();
             let mut compressed_bytes: Vec<u8> = Vec::new();
             for token in tokens {
-                if token.len() > 2 && is_valid_capitalization(token) && word_table.contains_key(&token.to_lowercase()){
-                    println!("compressing: {}",token);
+                if token.len() > 2 && is_valid_capitalization(token) && word_to_index.contains_key(&token.to_lowercase()){
 
                     // encode and store word
                         // bytes that store compressed word
                         // first bit is 1 to signify that this is a compressed word
                         let mut word_bytes: [u8; 3] = [0, 0, 0];
-                        let word_index = word_table[&token.to_lowercase()].as_u64().unwrap();
-
-                        println!("word_index: {}", word_index);
+                        let word_index = word_to_index[&token.to_lowercase()].as_u64().unwrap();
 
                         word_bytes[2] = word_index as u8;
                         word_bytes[1] = (word_index >> 8) as u8;
@@ -154,12 +212,10 @@ fn main() {
                     // signals the start of a compressed word
                         word_bytes[0] = word_bytes[0] | 0b10000000;
 
-                    println!("{:?}",word_bytes);
-
                     for byte in word_bytes {
                         compressed_bytes.push(byte);
                     }
-                    intermediate_compressed_bytes.push(vec![word_bytes[0].clone(),word_bytes[1].clone(),word_bytes[2].clone()]);
+                    intermediate_compressed_bytes.push(vec![word_bytes[0],word_bytes[1],word_bytes[2]]);
                 } else {
                     // append token to file as plaintext
                     for byte in token.bytes(){
@@ -171,7 +227,10 @@ fn main() {
 
         // print compressed bytes
 
-            println!("{:?}",intermediate_compressed_bytes)
+            // println!("{:?}",intermediate_compressed_bytes);
+
+            let decompressed = decompress(compressed_bytes, index_to_word);
+            println!("{}",decompressed);
             
 
 }
