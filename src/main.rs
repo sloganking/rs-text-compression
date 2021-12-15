d81a7a038851270c3c6491967f547ff89be5688a
Logan King <seanloganking@gmail.com>
2021-12-14 22:27:02 -0400

    partially compress message
    currently not encoding spaces to side of compressed word

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,38 @@
         use serde_json::json;
 
 // function declarations
+    fn encode_word(word: &str){
+
+    }
+
+    fn is_valid_capitalization(word: &str) -> bool{
+        if word == word.to_uppercase(){
+            return true
+        }
+        if word == word.to_lowercase(){
+            return true
+        }
+        // println!("word: {}",word);
+        // println!("len: {}",word.chars().count());
+        if word.chars().count() < 2 {
+            return true
+        }
+        // if first char is uppercase, and rest are lowercase
+        if &word[0..1] == word[0..1].to_uppercase() && &word[1..] == &word[1..].to_lowercase(){
+            return true
+        }
+        return false
+    }
+
+
+    fn decompress(compressed_bytes: Vec<u8>) -> String{
+
+        return "Hi".to_string();
+    }
 
 fn main() {
 
-    // prepair word table
+    // prepair word_table
         // retrieve words from json
             let mut file = File::open("./english-words/words.txt").expect("Failed to open file");
             let mut contents = String::new();
@@ -30,22 +58,15 @@ fn main() {
                 line.make_ascii_lowercase();
             }
 
-        // put words into table (for faster searching)
-            let mut table: serde_json::Value = serde_json::from_str("{}").expect("JSON was not well-formatted");
-            let table = table.as_object_mut().unwrap();
+        // put words into word_table (for faster searching)
+            let mut word_table: serde_json::Value = serde_json::from_str("{}").expect("JSON was not well-formatted");
+            let word_table = word_table.as_object_mut().unwrap();
             for (i, line) in fixed_lines.iter().enumerate() {
-                table.insert(line.to_string(), json!(i));
+                word_table.insert(line.to_string(), json!(i));
             }
-
-        // test if key in table
-            let key = "squid";
-            println!("key table: {}", table[key]);
+            let word_table = word_table;
 
         println!("done pre-processing");
-        
-
-
-
 
     // compress
 
@@ -54,16 +75,103 @@ fn main() {
             let mut contents = String::new();
             file.read_to_string(&mut contents).expect("Failed to read to string");
 
-        let to_compress = contents;
+        // crash if non ascii(< 127) character
+            for char in contents.chars() {
+                if char as u32 > 127 {
+                    panic!("Can't compress non ASCII character.")
+                }
+            }
 
-        // process input text
-            // turn string into tokens
+        // split strings to tokens (seperator is any character that's not alphanumeric, or '\'')
+            let mut result = Vec::new();
+            let mut last = 0;
+            for (index, matched) in contents.match_indices(|c: char| !(c.is_alphanumeric() || c == '\'')) {
+                if last != index {
+                    result.push(&contents[last..index]);
+                }
+                result.push(matched);
+                last = index + matched.len();
+            }
+            if last < contents.len() {
+                result.push(&contents[last..]);
+            }
 
-            let to_compress = to_compress.split(" ");
+            let tokens = result;
 
-            println!("{:?}",to_compress)
+        // turn string into tokens
+            // seperate by " "
+                // let current_tokens: Vec<&str> = contents.split([' ', '\n']).collect();
+            // re-add whitespace
+                // let mut next_tokens: Vec<&str> = Vec::new();
+                // for token in current_tokens{
+                //     if token.len() > 0 {
+                //         next_tokens.push(token);
+                //     }
+                //     next_tokens.push(" ");
+                // }
+                // next_tokens.pop();
 
+                // let tokens = next_tokens;
+
+
+        // test print tokens
+            // for x in 0..30{
+            //     println!("{}",tokens[x]);   
+            // }
             
+                
+        // compess tokens into bytes
+            let mut intermediate_compressed_bytes: Vec<Vec<u8>> = Vec::new();
+            let mut compressed_bytes: Vec<u8> = Vec::new();
+            for token in tokens {
+                if token.len() > 2 && is_valid_capitalization(token) && word_table.contains_key(token){
+                    println!("compressing: {}",token);
 
+                    // encode and store word
+                        // bytes that store compressed word
+                        // first bit is 1 to signify that this is a compressed word
+                        let mut word_bytes: [u8; 3] = [0, 0, 0];
+                        let word_index = word_table[token].as_u64().unwrap();
+
+                        println!("word_index: {}", word_index);
+
+                        word_bytes[2] = word_index as u8;
+                        word_bytes[1] = (word_index >> 8) as u8;
+                        word_bytes[0] = (word_index >> 16) as u8 & 0b00000111;
+
+                    // store case of word
+                        if token == token.to_lowercase(){
+                            // bits are 00
+                        } else if token == token.to_uppercase(){
+                            word_bytes[0] = word_bytes[0] | 0b00100000
+                        } else if token[0..1] == token[0..1].to_uppercase() && token[1..] == token[1..].to_lowercase(){
+                            word_bytes[0] = word_bytes[0] | 0b01000000
+                        } else {
+                            panic!("token \"{}\" slipped past is_valid_capitalization()", token);
+                        }
+
+                    // make first bit a 1
+                    // signals the start of a compressed word
+                        word_bytes[0] = word_bytes[0] | 0b10000000;
+
+                    println!("{:?}",word_bytes);
+
+                    for byte in word_bytes {
+                        compressed_bytes.push(byte);
+                    }
+                    intermediate_compressed_bytes.push(vec![word_bytes[0].clone(),word_bytes[1].clone(),word_bytes[2].clone()]);
+                } else {
+                    // append token to file as plaintext
+                    for byte in token.bytes(){
+                        compressed_bytes.push(byte);
+                        intermediate_compressed_bytes.push(vec![byte])
+                    }
+                }
+            }
+
+        // print compressed bytes
+
+            println!("{:?}",intermediate_compressed_bytes)
+            
 
 }
